552f2f46f4415128736431c1ef747a3b3cdd42aa
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-01-20 20:21:39 +0000

    Forms created and flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# GET and POST in method means that the py file will accept GET and POST requests from the html form 
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    # data = request.form
+    # this will print ImmutableMultiDict([('email', 'tim@gmail.com'), ('password', 'hshhshsh')])
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return "<p> logout </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        # Getting the information from the form
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error') 
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 # Storing standard roots for the website 
 # Home page, Log in page, etc.
 # Blueprint means it will have a bunch of routs inside
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 # The function runs when we at home page
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
